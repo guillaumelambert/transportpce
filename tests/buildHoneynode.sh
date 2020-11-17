10c0d3e9a46df8c62b370d01a4bab6eb7d3d9fad
Michael DÜrre <michael.duerre@highstreet-technologies.com>
2020-11-17 12:49:17 +0100

    fixed honeynode build script

--- tests/buildHoneynode.sh
+++ tests/buildHoneynode.sh
@@ -2,9 +2,9 @@
 
 ORDMVERSION=${1:-1.2.1}
 
-set -e
-. $(dirname $0)/reflectwarn.sh
-cd honeynode/$ORDMVERSION
-mvn clean install -DskipTests -Dcheckstyle.skip -Dmaven.javadoc.skip=true -s ../fd_io_honeycomb_settings.xml -q -V
-chmod +x ./honeynode-distribution/target/honeynode-distribution-1.19.04-hc/honeynode-distribution-1.19.04/honeycomb-tpce
+#set -e
+#. $(dirname $0)/reflectwarn.sh
+#cd honeynode/$ORDMVERSION
+mvn clean install -DskipTests -Dcheckstyle.skip -Dmaven.javadoc.skip=true -s honeynode/fd_io_honeycomb_settings.xml -f honeynode/$ORDMVERSION
+chmod +x ./honeynode/$ORDMVERSION/honeynode-distribution/target/honeynode-distribution-1.19.04-hc/honeynode-distribution-1.19.04/honeycomb-tpce
 exit $?
